d037314cc184c29316d80361e6d5939f6cccb1e8
Daniel Laera <laera.daniel@hotmail.com>
2022-03-20 19:00:48 +0000

    migrate schema & insert draft

--- db_migrations/V1__Initial.sql
+++ db_migrations/V1__Initial.sql
@@ -20,3 +20,15 @@ CREATE TABLE public.post (
 
 COMMENT ON TABLE public.post IS
 'Forum posts written by a user.';
+
+/*Create some dummy users*/
+INSERT INTO public.user (username) VALUES
+('Benjie'),
+('Singingwolfboy'),
+('Lexius');
+
+/*Create some dummy posts*/
+INSERT INTO public.post (title, body, author_id) VALUES
+('First post example', 'Lorem ipsum dolor sit amet', 1),
+('Second post example', 'Consectetur adipiscing elit', 2),
+('Third post example', 'Aenean blandit felis sodales', 3);
