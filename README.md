99ff9aa4297a853622ea0ee6cc5fd226a80aa3c9
Daniel Laera <laera.daniel@hotmail.com>
2022-03-20 18:52:12 +0000

    refactor & remove knexjs

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # PostGraphile-React-PostgreSQL-Boilerplate
-
-## migrate knex `npx knex migrate:latest`
-
-## seed db `npx knex seed:run`
--- db/knex.js
+++
@@ -1,4 +0,0 @@
-const environment = process.env.NODE_ENV || "development";
-const config = require("../knexfile")[environment];
-console.log(config);
-module.exports = require("knex")(config);
