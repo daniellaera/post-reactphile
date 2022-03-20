4e7e6cc6a032c82a5043c7d30dc9232937fc83e3
Daniel Laera <laera.daniel@hotmail.com>
2022-03-20 18:04:43 +0000

    graphiel conf

--- src/postgraphile.js
+++ src/postgraphile.js
@@ -2,9 +2,9 @@ const { postgraphile } = require("postgraphile");
 
 module.exports = postgraphile(
   {
-    database: "postgres",
+    database: "post-reactphile",
     user: "postgres",
-    password: "postgres",
+    password: "postgres_dev",
     host: "localhost",
     port: 5432,
   },
