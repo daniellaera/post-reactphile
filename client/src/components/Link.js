6463803f134f9fb3922a556c030a25fc06cade1e
Daniel Laera <laera.daniel@hotmail.com>
2022-03-26 18:33:43 +0000

    so we have a simple form

--- client/src/components/Link.js
+++ client/src/components/Link.js
@@ -1,4 +1,4 @@
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useMutation } from "@apollo/client";
 
 const FEED_QUERY = gql`
   query GetPosts {
@@ -15,20 +15,69 @@ const FEED_QUERY = gql`
   }
 `;
 
+const ADD_TODO = gql`
+  mutation CreatePost($title: String!, $body: String!, $authorId: Int!) {
+    createPost(
+      input: { post: { title: $title, body: $body, authorId: $authorId } }
+    ) {
+      post {
+        title
+        body
+      }
+    }
+  }
+`;
+
 export const Link = () => {
-  const { data } = useQuery(FEED_QUERY);
+  // const { data } = useQuery(FEED_QUERY);
+  let title;
+  let body;
+  const [addTodo, { data, loading, error }] = useMutation(ADD_TODO);
+
+  if (loading) return "Submitting...";
+  if (error) return `Submission error! ${error.message}`;
 
   return (
-    <div>
-      {data && (
-        <>
-          {data.allPosts.nodes.map((post) => (
-            <p key={post.id}>
-              {post.id} - {post.title}
-            </p>
-          ))}
-        </>
-      )}
-    </div>
+    <>
+      {/*  <div>
+        {data && (
+          <>
+            {data.allPosts.nodes.map((post) => (
+              <p key={post.id}>
+                {post.id} - {post.title}
+              </p>
+            ))}
+          </>
+        )}
+      </div> */}
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            addTodo({
+              variables: {
+                body: body.value,
+                title: title.value,
+                authorId: 2,
+              },
+            });
+            title.value = "";
+            body.value = "";
+          }}
+        >
+          <input
+            ref={(node) => {
+              title = node;
+            }}
+          />
+          <input
+            ref={(node) => {
+              body = node;
+            }}
+          />
+          <button type="submit">Add Todo</button>
+        </form>
+      </div>
+    </>
   );
 };
